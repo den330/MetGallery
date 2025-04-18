bb7fc2828315af1d6dfc329200515af6649d0add
den330 <wo131485508@qq.com>
2025-04-18 08:29:55 -0400

    Initial Commit

---
+++ MetGallery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetGallery
+//
+//  Created by yaxin on 2025-04-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetGallery/MetGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetGalleryApp.swift
+//  MetGallery
+//
+//  Created by yaxin on 2025-04-18.
+//
+
+import SwiftUI
+
+@main
+struct MetGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
